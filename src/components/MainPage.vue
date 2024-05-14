8d028391d687024c455a03e4f79cff49689f342c
redstone <che06189@gmail.com>
2024-05-14 11:15:53 +0900

    Refactor: 메인페이지를 변경된 디자인으로 수정

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -6,6 +6,11 @@
           <p class="text-white text-5xl font-kbizH">MoodMovie</p>
           <p class="text-white text-5xl font-kbizH mt-10">오늘의 기분을 달래보세요</p>
         </div>
+        <span
+          class="absolute bottom-20 left-1/2 material-symbols-outlined text-white text-7xl font-bold animate-bounce"
+        >
+          keyboard_double_arrow_down
+        </span>
       </div>
     </main>
   </div>
