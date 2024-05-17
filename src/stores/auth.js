1e6c5f788c8602ee25a68c242e39f4c12341d8bc
redstone <che06189@gmail.com>
2024-05-17 13:13:24 +0900

    Feat: 회원가입기능 backend와 연동

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -6,6 +6,13 @@ export const useAuthStore = defineStore(
   'auth',
   () => {
     const errorMsg = ref('')
+    const signUpErrorMsg = ref({
+      username: '',
+      nickname: '',
+      password1: '',
+      password2: '',
+      privacy: '',
+    })
     const token = ref(null)
 
     const userLogin = async (formValue) => {
@@ -28,16 +35,45 @@ export const useAuthStore = defineStore(
         return true
       } catch (error) {
         console.log(error)
+
         errorMsg.value = error.response.data.message[0]
         return false
       }
     }
-    const userRegister = (formValue) => {
+
+    const userRegister = async (formValue) => {
+      Object.keys(signUpErrorMsg.value).forEach((key) => {
+        signUpErrorMsg.value[key] = ''
+      })
       console.log(formValue)
-      // axios.post()
+
+      if (formValue.isPosChecked === false || formValue.isPrivacyChecked === false) {
+        signUpErrorMsg.value.privacy = '이용약관에 동의해주세요'
+        return
+      }
+      try {
+        const URL = import.meta.env.VITE_BACKEND_URL
+        const res = await axios({
+          method: 'post',
+          url: URL + '/accounts/registration/',
+          data: formValue,
+        })
+        console.log(res)
+        token.value = res.data.key
+        return true
+      } catch (error) {
+        // console.log(error)
+        Object.keys(error.response.data).forEach((key) => {
+          signUpErrorMsg.value[key] = error.response.data[key][0]
+        })
+        if (error.response.data['non_field_errors']) {
+          signUpErrorMsg.value.password2 = error.response.data['non_field_errors'][0]
+        }
+        return false
+      }
     }
 
-    return { token, errorMsg, userLogin, userRegister }
+    return { token, errorMsg, signUpErrorMsg, userLogin, userRegister }
   },
   { persist: [{ paths: ['token'], storage: sessionStorage }] },
 )
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -7,26 +7,34 @@
           <div class="my-10">
             <p class="font-summer text-5xl text-mint">Sign Up</p>
           </div>
-          <form class="font-kbizR" @submit.prevent="fetchLogin">
-            <!-- userId form -->
+          <form class="font-kbizR" @submit.prevent="fetchRegister">
+            <!-- username form -->
             <div class="mb-1 flex flex-col justify-between">
               <label class="text-mint text-2xl py-2" for="userId">아이디</label>
               <input
                 id="userId"
                 class="w-[395px] h-[48px] pl-2 rounded-sm"
                 placeholder="아이디를 입력해주세요"
-                v-model="userId"
+                v-model="formData.username"
               />
             </div>
-            <!-- username form -->
+            <!-- error MSG -->
+            <div class="text-red-300 text-sm w-[395px]">
+              <p>{{ store.signUpErrorMsg.username }}</p>
+            </div>
+            <!-- nickname form -->
             <div class="mb-1 flex flex-col justify-between">
               <label class="text-mint text-2xl py-2" for="username">닉네임</label>
               <input
                 id="username"
                 class="w-[395px] h-[48px] pl-2 rounded-sm"
                 placeholder="닉네임을 입력해주세요"
-                v-model="username"
+                v-model="formData.nickname"
               />
+            </div>
+            <!-- error MSG -->
+            <div class="text-red-300 text-sm w-[395px]">
+              <p>{{ store.signUpErrorMsg.nickname }}</p>
             </div>
             <!-- password form -->
             <div class="flex flex-col justify-between">
@@ -37,28 +45,53 @@
                 type="password"
                 autoComplete="off"
                 placeholder="비밀번호를 입력해주세요"
-                v-model="password"
+                v-model="formData.password1"
               />
             </div>
             <!-- error MSG -->
-            <div class="flex justify-center text-red-300 font-medium text-lg my-3 w-[395px]">
-              <p>{{ errorMsg }}</p>
+            <div class="text-red-300 text-sm w-[395px]">
+              <p>{{ store.signUpErrorMsg.password1 }}</p>
+            </div>
+            <!-- confirm_password form -->
+            <div class="flex flex-col justify-between">
+              <label class="text-mint text-2xl py-2" for="password_confirm">비밀번호 확인</label>
+              <input
+                id="password_confirm"
+                class="w-[395px] h-[48px] pl-2 rounded-sm"
+                type="password"
+                autoComplete="off"
+                placeholder="비밀번호를 다시 입력해주세요"
+                v-model="formData.password2"
+              />
+            </div>
+            <!-- error MSG -->
+            <div class="text-red-300 text-sm w-[395px]">
+              <p>{{ store.signUpErrorMsg.password2 }}</p>
             </div>
             <!-- privacy -->
-            <div>
+            <div class="mt-3">
               <div>
-                <input type="checkbox" id="tos" value="tos" v-model="isPosChecked" />
+                <input type="checkbox" id="tos" value="tos" v-model="formData.isPosChecked" />
                 <label class="text-white text-base ml-2" for="tos"
                   >이용약관 동의(필수)
                   <span class="underline underline-offset-2 text-sm">자세히</span>
                 </label>
               </div>
               <div>
-                <input type="checkbox" id="privacy" value="privacy" v-model="isPrivacyChecked" />
+                <input
+                  type="checkbox"
+                  id="privacy"
+                  value="privacy"
+                  v-model="formData.isPrivacyChecked"
+                />
                 <label class="text-white text-base ml-2" for="privacy"
                   >개인정보 수집 및 이용동의(필수)
                   <span class="underline underline-offset-2 text-sm">자세히</span>
                 </label>
+              </div>
+              <!-- error MSG -->
+              <div class="text-red-300 text-sm w-[395px]">
+                <p>{{ store.signUpErrorMsg.privacy }}</p>
               </div>
             </div>
 
@@ -79,43 +112,26 @@
 
 <script setup>
 import GlobalButton from '@/components/GlobalButton.vue'
+import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 import { ref } from 'vue'
 
 const store = useAuthStore()
-const userId = ref('')
-const username = ref('')
-const password = ref('')
-const isPosChecked = ref(false)
-const isPrivacyChecked = ref(false)
-const errorMsg = ref('')
+const router = useRouter()
 
-const fetchLogin = () => {
-  errorMsg.value = ''
-  if (isPosChecked.value === false || isPrivacyChecked.value === false) {
-    errorMsg.value = '이용약관에 동의해주세요.'
-    return
+const formData = ref({
+  username: '',
+  nickname: '',
+  password1: '',
+  password2: '',
+  isPosChecked: false,
+  isPrivacyChecked: false,
+})
+
+const fetchRegister = async () => {
+  const status = await store.userRegister(formData.value)
+  if (status === true) {
+    router.push({ name: 'home' })
   }
-
-  if (userId.value === '' || password.value === '' || username.value === '') {
-    errorMsg.value = '빈칸이 존재합니다.'
-    userId.value = ''
-    password.value = ''
-    username.value = ''
-    return
-  }
-
-  const data = {
-    userId: userId.value,
-    username: username.value,
-    password: password.value,
-  }
-
-  store.userLogin(data)
-  userId.value = ''
-  password.value = ''
-  username.value = ''
 }
 </script>
-
-<style scoped></style>
