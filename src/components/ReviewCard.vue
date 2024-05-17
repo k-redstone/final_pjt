8cbb01a678f3ca4032f6da6fbfb4099ae98f007c
redstone <che06189@gmail.com>
2024-05-17 19:26:52 +0900

    Feat: 자유게시판 댓글작성 기능 구현

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="flex flex-col gap-y-2">
     <div class="flex gap-x-3">
-      <RouterLink :to="{ name: 'profile', params: { username: '유저이름' } }">
-        <P>유저이름</P>
+      <RouterLink :to="{ name: 'profile', params: { username: comment.userId } }">
+        <p>{{ comment.userId }}</p>
       </RouterLink>
-      <p class="text-gray-400">시간</p>
+      <p class="text-gray-400">{{ comment.created_at }}</p>
     </div>
-    <p>comment</p>
+    <p>{{ comment.content }}</p>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  comment: Object,
+})
+</script>
--- src/pages/community/CommunityDetailPage.vue
+++ src/pages/community/CommunityDetailPage.vue
@@ -7,18 +7,15 @@
       <div class="bg-white rounded-lg px-5 pt-7 pb-3 my-6 font-kbizR">
         <!-- 유지명 및 기능 -->
         <div class="flex items-center gap-x-4">
-          <span class="text-2xl font-kbizB">유저이름</span>
-          <span class="text-lg grow text-gray-400">시간</span>
+          <span class="text-2xl font-kbizB">{{ postData.article?.userId }}</span>
+          <span class="text-lg grow text-gray-400">{{ postData.article?.created_at }}</span>
           <span class="text-lg">수정</span>
           <span class="text-lg">삭제</span>
         </div>
         <!-- 본문 -->
         <div class="px-20 py-5">
           <p>
-            이 영화 강추합니다.이 영화 강추합니다.이 영화 강추합니다.이 영화 강추합니다.이 영화
-            강추합니다.이 영화 강추합니다.이 영화 강추합니다.이 영화 강추합니다.이 영화
-            강추합니다.이 영화 강추합니다.이 영화 강추합니다.이 영화 강추합니다.이 영화
-            강추합니다.이 영화 강추합니다.이 영화 강추합니다.이 영화 강추합니다.
+            {{ postData.article?.content }}
           </p>
         </div>
       </div>
@@ -26,9 +23,10 @@
         class="bg-white rounded-lg mt-10 p-6 text-black flex flex-col gap-y-3 mb-20 overflow-y-auto"
       >
         <h3 class="text-2xl">댓글</h3>
-        <form class="border p-3 border-black" @submit.prevent="submitComment">
+        <form class="border p-3 border-black" @submit.prevent="submitComment(postData.article.id)">
           <p>유저이름</p>
           <textarea
+            id="comment"
             type="text"
             class="h-10 w-full overflow-y-auto resize-none"
             placeholder="댓글을 달아주세요."
@@ -41,8 +39,8 @@
           </div>
         </form>
         <div>
-          <div v-for="num in 10">
-            <ReviewCard class="py-5" />
+          <div v-for="comment in postData?.comments" :key="comment.id">
+            <ReviewCard class="py-5" :comment="comment" />
             <hr />
           </div>
         </div>
@@ -54,38 +52,80 @@
 <script setup>
 import GlobalButton from '@/components/GlobalButton.vue'
 import ReviewCard from '@/components/ReviewCard.vue'
+import axios from 'axios'
+import { useAuthStore } from '@/stores/auth'
+import { useRoute } from 'vue-router'
+import { ref, computed, onMounted } from 'vue'
 
-import { ref, computed } from 'vue'
+const store = useAuthStore()
+const route = useRoute()
+const comment = ref({
+  content: '',
+})
+const postData = ref({})
 
-// const articleData = ref({})
-const comment = ref('')
+onMounted(() => {
+  getPostDetail()
+})
+
+const getPostDetail = () => {
+  const URL = import.meta.env.VITE_BACKEND_URL
+  const headers = {
+    Authorization: `Token ${store.token}`,
+  }
+
+  axios({
+    method: 'get',
+    url: URL + `/free_board/${route.params.postId}/`,
+    headers: headers,
+  }).then((res) => {
+    postData.value = res.data
+  })
+}
 
 const changeReview = (event) => {
   if (event.inputType === 'deleteContentBackward') {
-    comment.value = event.target.value
+    comment.value.content = event.target.value
     return
   }
   if (event.inputType === 'insertFromPaste') {
     if (event.target.value.length >= 100) {
       alert('100자까지 입력할 수 있습니다.')
-      event.target.value = comment.value
+      event.target.value = comment.value.content
       return
     }
   }
   if (commentLength.value >= 100) {
     alert('100자까지 입력할 수 있습니다.')
-    event.target.value = comment.value
+    event.target.value = comment.value.content
     return
   }
-  comment.value = event.target.value
+  comment.value.content = event.target.value
 }
 
-const submitComment = () => {
-  alert('댓글달기')
+const submitComment = (postId) => {
+  const URL = import.meta.env.VITE_BACKEND_URL
+  const headers = {
+    Authorization: `Token ${store.token}`,
+  }
+  axios({
+    method: 'post',
+    url: URL + `/free_board/${postId}/comment/`,
+    headers: headers,
+    data: comment.value,
+  })
+    .then(() => {
+      document.querySelector('#comment').value = ''
+      comment.value.content = ''
+      getPostDetail()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
 const commentLength = computed(() => {
-  return comment.value.length
+  return comment.value.content.length
 })
 </script>
 
